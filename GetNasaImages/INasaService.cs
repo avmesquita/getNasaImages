3b5a54e4a067b7cc2b3a7f4b96ec3af8a7c717a6
avmesquita <avmesquita@gmail.com>
2020-06-16 13:52:16 +0100

    include interface

---
+++ GetNasaImages/INasaService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GetNasaImages
+{
+	public interface INasaService
+	{
+		NasaAPOD getAPOD(bool isHD = false, DateTime? date = null);
+
+		byte[] getAstronomicPicOfDay(bool isHD = false, DateTime? date = null);		
+	}
+}
